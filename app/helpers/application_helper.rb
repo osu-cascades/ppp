2ebb633fc251c449f1c9d0f8bba14b00adc97d29
Yong Bakos <ybakos@humanoriented.com>
2017-02-26 08:49:50 -0800

    Polish the main layout. - add language attribute - incorporate haml page_class helper - extract responsive navbar button attribtues into helper - soften quotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def navbar_button_attributes
+    {type: 'button', 'data-toggle': 'collapse', 'data-target': '#navbarResponsive', 'aria-controls': 'navbarResponsive', 'aria-expanded': 'false', 'aria-label': 'Toggle navigation'}
+  end
+
 end
